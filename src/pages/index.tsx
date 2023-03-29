13c2355df7e30118c05010fe2c5a12d479643e6d
ginozega8 <ginozega8@outlook.es>
2023-03-29 18:12:42 -0300

    Added circle animation :lipstick:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,23 @@ export default function Home() {
   return <div>
     <Navbar />
     <div className={styles.homediv}>
-      <div className='flex flex-row place-content-evenly pt-[180px] max-lg:pt-[20px] max-lg:flex-col-reverse max-lg:items-center'>
+      <div className='flex flex-row place-content-evenly pt-[180px] max-lg:pt-[20px] max-lg:flex-col-reverse max-lg:items-center relative'>
         <div className='max-lg:flex-col max-lg:text-center'>
-        <h1>GINO ZEGA <br /> FRONTEND DEVELOPER</h1>
-        <hr className='max-md:w-[280px]'></hr>
+          <h1>GINO ZEGA <br /> FRONTEND DEVELOPER</h1>
+          <hr className='max-md:w-[280px]'></hr>
         </div>
-        <div>
-        <Image
-          src="https://res.cloudinary.com/dgiqi5urn/image/upload/v1680106020/Portfolio%202023/material_mtvlih.png"
-          alt="Cube"
-          width={350}
-          height={350}
-          unoptimized
-          className='max-lg:w-[250px]'
-        />
+        <div className='relative'>
+          <Image
+            src="https://res.cloudinary.com/dgiqi5urn/image/upload/v1680106020/Portfolio%202023/material_mtvlih.png"
+            alt="Cube"
+            width={350}
+            height={350}
+            unoptimized
+            className='max-lg:w-[250px]'
+          />
+          <div className="main">
+            <div className="circle"></div>
+          </div>
         </div>
       </div>
     </div>
