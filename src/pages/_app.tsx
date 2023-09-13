03b7733d05cd0c3f51935b1a58678db5a759a1a1
ginozega8 <ginozega8@outlook.es>
2023-09-13 14:04:56 -0300

    Changed page name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react';
 import LoadingScreen from '../Components/LoadingScreen';
 import { motion } from 'framer-motion';
-import About from './about';
 
 export default function App({ Component, pageProps }: AppProps) {
   // Loading Screen Functionality
